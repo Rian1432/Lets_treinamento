565414d92a3417ac7faf987957128451905ceaac
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2021-12-28 13:08:52 -0300

    viacep colocado no cadastro

---
+++ projeto/viacep.js
@@ -0,0 +1,27 @@
+/*modo do Javascript que ajuda a escrever um código mais limpo, ele impede que use variáveis sem as declarar antes e ,ais algumas outras regras.*/
+'use strict';
+
+//função que preenche usando os dados da API
+const preencherFormulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+//função que pega os dados da API e o trás em json
+const pesquisarCep = async() => {
+    const cep = document.getElementById('cep').value;
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+
+    if(endereco.hasOwnProperty('erro')){
+        document.getElementById('rua').value = 'CEP não encontrado!';
+    }else{
+        preencherFormulario(endereco);
+    }
+}
+
+//inicializador de pesquisa(faz as funções acima funcionarem)
+document.getElementById('cep').addEventListener('focusout',pesquisarCep);
